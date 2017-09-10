67c8eb796e89de0949bd233dfe183b8605baca4e
Gerard Hickey <Gerard.Hickey@smartsheet.com>
2017-09-10 11:34:16 -0700

    Reduced distances to less than 100,000 miles

--- nasa/src/index.js
+++ nasa/src/index.js
@@ -2,16 +2,17 @@
 var Alexa = require('alexa-sdk');
 var Https = require('https');
 var Url = require('url');
+var Util = require('util');
 //var Math = require('math');
 
 
-//Replace with your app ID (OPTIONAL).  You can find this value at the top of your skill's page on http://developer.amazon.com.  
+//Replace with your app ID (OPTIONAL).  You can find this value at the top of your skill's page on http://developer.amazon.com.
 //Make sure to enclose your value in quotes, like this: var APP_ID = "amzn1.ask.skill.bb4045e6-b3e8-4133-b650-72923c5980f1";
 var APP_ID = "amzn1.ask.skill.078f9a52-af80-47b3-b564-44a1282a8d72";
 
-var HELP_MESSAGE = "You can say tell me a space fact, or, you can say exit... What can I help you with?";
+var HELP_MESSAGE = "I am able to search for objects approaching the earth over the next week. Would you like me to search now? ";
 var HELP_REPROMPT = "What can I help you with?";
-var STOP_MESSAGE = "Goodbye!";
+var STOP_MESSAGE = "Goodbye! I hope that there will be no impacts and we will be able to speak again.";
 
 var alexa;
 var APIKey = "jMf9vfPfGRmf81ncsaoZIhpLPMJPFRd8mJjdqwkw";
@@ -20,18 +21,22 @@ var AsteroidURL = "https://api.nasa.gov/neo/rest/v1/feed?start_date=%DATE%&api_k
 
 
 //=========================================================================================================================================
-//Editing anything below this line might break your skill.  
+//Editing anything below this line might break your skill.
 //=========================================================================================================================================
 exports.handler = function(event, context, callback) {
     alexa = Alexa.handler(event, context);
-    alexa.APP_ID = APP_ID;
+    console.log("event = %s", Util.inspect(event, true, null));
+    console.log("context = %s", Util.inspect(context, true, null));
+    console.log("callback = %s", Util.inspect(callback, true, null));
+
+    //alexa.appId = APP_ID;
     alexa.registerHandlers(handlers);
     alexa.execute();
 };
 
 var handlers = {
     'LaunchRequest': function () {
-        this.emit('GetNewFactIntent');
+        alexa.emit(':ask', HELP_MESSAGE);
     },
     'GetCloseCalls': function () {
         var url = buildUrl();
@@ -51,10 +56,10 @@ var handlers = {
                         console.log("Checking %s", date);
                         for (let obj of data.near_earth_objects[date]) {
                             console.log("distance = ", obj.close_approach_data[0].miss_distance.miles);
-                            if (parseInt(obj.close_approach_data[0].miss_distance.miles) < 1000000) {
+                            if (parseInt(obj.close_approach_data[0].miss_distance.miles) < 1000) {
                                 console.log("near miss");
                                 hit1m.push("On " + date + " Asteroid " + obj.name + " will miss by " + nice_number(obj.close_approach_data[0].miss_distance.miles) +" miles; ");
-                            } else if (parseInt(obj.close_approach_data[0].miss_distance.miles) < 7000000) {
+                            } else if (parseInt(obj.close_approach_data[0].miss_distance.miles) < 100000) {
                                 console.log("close call");
                                 hit7m.push("On " + date + " Asteroid " + obj.name + " will miss by " + nice_number(obj.close_approach_data[0].miss_distance.miles) +" miles; ");
                             }
@@ -62,20 +67,23 @@ var handlers = {
                     });
 
                     // put it all together
-                    output = "Out of " + data.element_count + " objects,\n\n";
-                    console.log("hit1m = " + hit1m.length);
-                    console.log("hit7m = " + hit7m.length);
-                    if (hit1m.length > 0) {
-                        output += "There are " + hit1m.length + " near misses;\n";
-                        for (var entry of hit1m) {
-                            output += entry;
+                    output = ''
+                    if (hit1m.length > 0 || hit7m.length > 0) {
+                        output = "Out of " + data.element_count + " objects,\n\n";
+                        if (hit1m.length > 0) {
+                            output += "There are " + hit1m.length + " near misses;\n";
+                            for (var entry of hit1m) {
+                                output += entry;
+                            }
                         }
-                    }
-                    if (hit7m.length > 0) {
-                        output += "There are " + hit7m.length + " approaches;\n";
-                        for (var entry of hit7m) {
-                            output += entry;
+                        if (hit7m.length > 0) {
+                            output += "There are " + hit7m.length + " approaches;\n";
+                            for (var entry of hit7m) {
+                                output += entry;
+                            }
                         }
+                    } else {
+                        output = "There are no approaching asteroids coming within one hundred thousand miles this week.";
                     }
                 }
 
@@ -87,13 +95,16 @@ var handlers = {
     'AMAZON.HelpIntent': function () {
         var speechOutput = HELP_MESSAGE;
         var reprompt = HELP_REPROMPT;
-        this.emit(':ask', speechOutput, reprompt);
+        alexa.emit(':ask', speechOutput, reprompt);
     },
     'AMAZON.CancelIntent': function () {
-        this.emit(':tell', STOP_MESSAGE);
+        alexa.emit(':tell', STOP_MESSAGE);
     },
     'AMAZON.StopIntent': function () {
-        this.emit(':tell', STOP_MESSAGE);
+        alexa.emit(':tell', STOP_MESSAGE);
+    },
+    'SessionEndedRequest': function () {
+        alexa.emit(':tell', STOP_MESSAGE);
     }
 };
 
